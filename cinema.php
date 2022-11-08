fdbd5c2d49c2b6cfd5a72ec998c65d7b09a2d639
JLoewenguth <jrm.lguth@gmail.com>
2022-11-08 16:31:13 +0100

    casting

--- cinema.php
+++ cinema.php
@@ -7,8 +7,9 @@ catch (Exception $e)
     die('Erreur : ' . $e->getMessage());
 }
 
-
-$cinemaStatement = $mySqlClient->prepare('SELECT id_film, titre, annee_sortie_france AS annee, nom, prenom, duree_minutes
+$cinemaStatement = $mySqlClient->prepare('SELECT id_film, titre,
+annee_sortie_france AS annee, nom, prenom,
+TIME_FORMAT(SEC_TO_TIME(duree_minutes*60), "%H:%i") as duree_heures
 FROM film f
 INNER JOIN realisateur r ON r.id_realisateur=f.id_realisateur
 INNER JOIN personne p ON p.id_personne=r.id_personne');
@@ -32,7 +33,7 @@ $cinema = $cinemaStatement->fetchAll();
             <tr>
                 <td><a href="detailFilm.php?id=<?= $cinema["id_film"] ?>"><?= $cinema["titre"] ?></a></td>
                 <td><?= $cinema["annee"] ?></td>
-                <td><?= $cinema["duree_minutes"] ?></td>
+                <td><?= $cinema["duree_heures"] ?></td>
                 <td><?= $cinema["nom"]." ".$cinema["prenom"] ?></td>
             </tr>
     <?php    } ?>
--- detailFilm.php
+++ detailFilm.php
@@ -24,12 +24,7 @@ $cinemaStatement = $pdo->prepare($sql);
 //execute associe le nom du champ param avec l'id de l'URL (ligne 3)
 $cinemaStatement->execute(["id"=>$id]);
 $cinema = $cinemaStatement->fetch();
-?>
 
-<h1><?= $cinema["titre"]; ?></h1>
-<p>Année de sortie : <?= $cinema["annee_sortie_france"] ?></p>
-
-<?php
 $sqlCasting = "SELECT concat(prenom,' ',nom) as acteur, nom_role
                 FROM personne p
                 inner join acteur a on p.id_personne = a.id_personne
@@ -38,13 +33,17 @@ $sqlCasting = "SELECT concat(prenom,' ',nom) as acteur, nom_role
                 WHERE j.id_film = :id";
 
 $castingStatement = $pdo->prepare($sqlCasting);
+$castingStatement->execute(["id"=>$id]);
 $casting = $castingStatement->fetch();
 ?>
+
+<h1><?= $cinema["titre"]; ?></h1>
+<p>Année de sortie : <?= $cinema["annee_sortie_france"] ?></p>
 
 <h3>Casting</h3>
 <ul>
 <?php
-    foreach($casting as $casting){ ?>
+    foreach($castingStatement as $casting){ ?>
         <li><?= $casting["acteur"] ?> (<?= $casting["nom_role"] ?>)</li>
 <?php } ?>
 </ul>
